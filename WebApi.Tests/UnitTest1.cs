146957962a5a46b609b13bfacfa2e606d43b0fce
Sven <bitbucket@frechi.de>
2025-12-08 00:56:23 +0100

    Added test project, moved api project

---
+++ WebApi.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit.Abstractions;
+
+namespace WebApi.Tests;
+
+public class BasicTests
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public BasicTests(WebApplicationFactory<Program> factory, ITestOutputHelper testOutputHelper)
+    {
+        _factory = factory;
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Theory]
+    [InlineData("/Test")]
+    public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(url);
+
+        _testOutputHelper.WriteLine("Hello Test World");
+
+        // Assert
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        Assert.Equal("application/json; charset=utf-8",
+            response.Content.Headers.ContentType.ToString());
+    }
+}
