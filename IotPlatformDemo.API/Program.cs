9ca930c8edc595eb2e55af3826f3cbea4e6fc240
Sven <bitbucket@frechi.de>
2025-12-27 18:52:39 +0100

    Swagger auth flow

--- IotPlatformDemo.API/Program.cs
+++ IotPlatformDemo.API/Program.cs
@@ -26,6 +26,39 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMi
 
 builder.Services.AddSwaggerGen(options =>
 {
+    var scopes = new Dictionary<string, string>
+    {
+        { configuration["SwaggerAuth:Scope"], "ReadWrite" },
+        // add any custom scopes necessary, in place of or in addition to Default, e.g.
+        // { $"api://{builder.Configuration["AzureAd:ClientId"]}/access_as_user", "Access as User" },
+        { "openid", "openid" },
+    };
+    var authFlow = new OpenApiOAuthFlow
+    {
+        AuthorizationUrl = new Uri($"https://svfriotdemo.ciamlogin.com/0484e3c3-e93f-4d52-bbb0-4af88d1c1b44/oauth2/v2.0/authorize"),
+        TokenUrl = new Uri($"https://svfriotdemo.ciamlogin.com/0484e3c3-e93f-4d52-bbb0-4af88d1c1b44/oauth2/v2.0/token"),
+        Scopes = scopes,
+    };
+    var openApiSecurityScheme = new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            //AuthorizationCode = authFlow,
+            Implicit = authFlow,
+        },
+        In = ParameterLocation.Header,
+        Scheme = "oauth2",
+        Name = "Authorization",
+        Description = "Use OAuth2 [Auth Code] authorization",
+    };
+
+    options.AddSecurityDefinition("bearer", openApiSecurityScheme);
+    options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
+        [new OpenApiSecuritySchemeReference("bearer", document)] = []
+    });
+
     options.SwaggerDoc(apiVersion, new ()
     {
         Title = apiTitle,
@@ -43,6 +76,10 @@ app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint($"/swagger/{apiVersion}/swagger.json", $"{apiTitle} {apiVersion}");
     options.RoutePrefix = string.Empty; // serve UI at application root
+    options.OAuthClientId(configuration["SwaggerAuth:ClientId"]);
+    options.OAuthClientSecret(configuration["SwaggerAuth:ClientSecret"]);
+    options.OAuthScopes(["openid", configuration["SwaggerAuth:Scope"]]);
+    options.OAuthUseBasicAuthenticationWithAccessCodeGrant();
 });
 
 app.UseHttpsRedirection();
